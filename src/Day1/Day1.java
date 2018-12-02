6fb2bc906f3affd9460a3ff0b9b93337db692cc8
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2018-12-01 20:26:13 -0500

    Day 1 Finished and some stuff got thrown into /out/

--- src/Day1/Day1.java
+++ src/Day1/Day1.java
@@ -1,13 +1,49 @@
 package Day1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+
 /**
  * 2018 Advent of Code Day 1
  * Challenge Completion
  * Author: Quinn Kleinfelter
- * Started coding at:
- * Completed at:
+ * Started coding at: 8EST
+ * Completed at: 8:24EST
  * Code time elapsed:
  */
 public class Day1 {
 
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner file = new Scanner(new File("C:/Users/Quinn/Documents/GitHub/AdventOfCode2018/src/Day1/day1input.txt"));
+        ArrayList<Integer> nums = new ArrayList<>();
+        while(file.hasNext()){
+            nums.add(Integer.parseInt(file.next()));
+        }
+        int freq = 0;
+        HashSet<Integer> freqs = new HashSet();
+        freqs.add(freq);
+        boolean freqFound = false;
+        int i = 0;
+        while(!freqFound){
+//        for(int i : nums){
+
+            freq += nums.get(i);
+            if(!freqs.contains(freq)){
+                freqs.add(freq);
+            }else{
+                System.out.println("reached " + freq + " twice");
+                freqFound = true;
+            }
+            if (i == nums.size() - 1) {
+                i = 0;
+            }else{
+                i++;
+            }
+        }
+        System.out.println(freq);
+    }
 }
